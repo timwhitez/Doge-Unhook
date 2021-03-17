833c532a19384d9855e638aeab2cc84b20e4e734
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-03-17 23:14:12 +0800

    Update main.go

--- FullDllunhook/main.go
+++ FullDllunhook/main.go
@@ -1,11 +1,21 @@
 package main
-
+import "C"
 import (
 	"log"
 )
 
 func main(){
-	err := RefreshPE(`c:\windows\system32\ntdll.dll`)
+	err := RefreshPE(`c:\windows\system32\kernel32.dll`)
+	if err != nil {
+		log.Println("RefreshPE failed:", err)
+	}
+
+	err = RefreshPE(`c:\windows\system32\kernelbase.dll`)
+	if err != nil {
+		log.Println("RefreshPE failed:", err)
+	}
+
+	err = RefreshPE(`c:\windows\system32\ntdll.dll`)
 	if err != nil {
 		log.Println("RefreshPE failed:", err)
 	}
